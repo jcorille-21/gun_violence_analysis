fba7323d47344844bccc4128886ba96be6d6b7ba
jcorille-21 <jcorille@uw.edu>
2022-05-03 02:47:52 -0700

    added

---
+++ analysis.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+# Analysis for Shootings of 2018 #
+
+
+# Dataframe showing shootings of 2018
+
+
+# Summary of Shootings #
+
+# Number of total shootings
+
+
+# Number of total deaths
+
+
+# Dataframe of the city with the most deaths
+
+
+# Most amount of deaths in a city
+
+
+# City with most amount of deaths
+
+
+# Dataframe of the state with most injuries
+
+
+
+# Scatterplot of 2018 Shootings (deaths and injuries) #
+
+# Scatter plot comparing number of deaths and injuries
